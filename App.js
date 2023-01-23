e50f6cadd14cd371fa25119c79c739484a5a936e
Samiha Hossain <75538711+Samiha-Hossain19@users.noreply.github.com>
2023-01-23 19:50:47 +0600

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, FlatList, Alert } from "react-native";
+import Header from "./Components/header";
+import TodoItem from "./Components/todos";
+import AddTodo from "./Components/addTodo";
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { text: "Learn React", key: "1" },
+    { text: "Learn Java", key: "2" },
+    { text: "Eat Dinner", key: "3" },
+  ]);
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.key != key);
+    });
+  };
+
+  const submitHandler = (text) => {
+    if (text.length > 2) {
+      setTodos((prevTodos) => {
+        return [{ text: text, key: Math.random().toString() }, ...prevTodos];
+      });
+    } else {
+      Alert.alert("Opps!", "Todos must be over two characters long", [
+        { text: "Ok", onPress: () => console.log("alert closed") },
+      ]);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header />
+      <View style={styles.content}>
+        <AddTodo submitHandler={submitHandler} />
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <TodoItem item={item} pressHandler={pressHandler} />
+            )}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +50,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
   },
 });
